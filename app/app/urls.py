074a3a4c3a96ecb709379811f7b0fa0d8faf7ef6
Valentinminolli <minolli.vale@gmail.com>
2023-01-17 12:29:17 -0300

    Added GitHub Actions.

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+URLMenPatterns = [
+    path('admin/', admin. sitio. urls),
 ]
+
+uno='1'
