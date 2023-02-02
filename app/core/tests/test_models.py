cfea5e7e6e28833bb076e940fabc4d3c2f1cc525
Valentinminolli <minolli.vale@gmail.com>
2023-02-02 15:35:10 -0300

    Update: Uncomplete Tag API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,6 +9,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_ussr(email='user@example.com', password='testpass123'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -70,3 +75,11 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_ussr()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
