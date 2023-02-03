4570b3fb7fb8f3883c811c1d8a9a4391c9530fda
Valentin Minolli <minolli.vale@gmail.com>
2023-02-02 23:43:32 -0300

    Adding Possible Solution.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -27,14 +27,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
 
-
-	def get_serializer_class(self):
-		"""Return serializer class for request."""
-    	if self.action == 'list':
+    def get_serializer_class(self):
+        """Return serializer class for request."""
+        if self.action == 'list':
             return serializers.RecipeSerializer
 
         return self.serializer_class
-
 
     def perform_create(self, serializer):
         """Create a new recipe."""
