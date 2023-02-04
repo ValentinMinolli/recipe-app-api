b9551ab0893fecb8b40bd9d499610476351c71f8
Valentin Minolli <minolli.vale@gmail.com>
2023-02-04 17:02:21 -0300

    Update: TAG API finished [Fixed].

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -272,7 +272,7 @@ class PrivateRecipeApiTests(TestCase):
 
     def test_clear_recipe_tags(self):
         """Test clearing a recipes tags."""
-        tag = Tag.objects.create(user=self.user, name = 'Dessert')
+        tag = Tag.objects.create(user=self.user, name='Dessert')
         recipe = create_recipe(user=self.user)
         recipe.tags.add(tag)
 
